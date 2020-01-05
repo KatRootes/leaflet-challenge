69bb43e2dc17a3f0b57ad570dfd57c03143acb8d
Katherine <53545200+KatRootes@users.noreply.github.com>
2020-01-05 15:23:30 -0600

    Earthquake layer added.  Need to make colors dynamic.

--- js/logic.js
+++ js/logic.js
@@ -1,10 +1,46 @@
+// USGS Earthquake Data
+//var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson";
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
 // Create map object and set default layers
-var myMap = L.map("map", {
-  center: [46.2276, 2.2137],
+var start = [38.8, -122.8];
+var usgsMap = L.map("map", {
+  center: start,
   zoom: 6,
-  layers: [light] //[light, cityLayer]
+  layers: [baseMaps.Light]
 });
+
+// Create a layer for earthquake data
+var earthquakeLayer = L.layerGroup().addTo(usgsMap);
+
+// Grab data with d3
+d3.json(geoData, function(data) 
+{
+    // Loop through data
+    for (var i = 0; i < data.features.length; i++) 
+    {
+      // Set the data location property to a variable
+      var location = data.features[i].geometry;
+      
+      // Check for location property
+      if (location) 
+      {
+        L.circle([location.coordinates[1],location.coordinates[0]], 
+        {
+          color: "red",
+          fillColor: "#f03",
+          fillOpacity: 0.5,
+          radius: 2000.0 * data.features[i].properties.mag
+        }).addTo(earthquakeLayer);
+      }
+   }
+});
+
+// Create a dictionary of overlays
+var overlayMaps = {
+    Earthquakes: earthquakeLayer
+};
 
 // Pass our map layers into our layer control
 // Add the layer control to the map
-L.control.layers(baseMaps).addTo(myMap); //, overlayMaps).addTo(myMap);
+L.control.layers(baseMaps, overlayMaps).addTo(usgsMap);
